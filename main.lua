e75179516adfd58293cd58ac4da74f06f1a961eb
MatuzawaGaku <amatu4933@gmail.com>
2016-06-28 11:47:47 +0900

    main.lua追加

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
 
+--main.lua
+-----------------------------------------------------------------------------------------
+
+-- hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- include the Corona "composer" module
+local composer = require "composer"
+
+-- load menu screen
+composer.gotoScene( "menu" )main.lua
+
